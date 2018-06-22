c6ca8c964f2aa67158c9f0cc9666f4bc995372c5
Florian Lenz <flenz81@googlemail.com>
2018-06-22 10:03:04 +0300

    [ethereum] added vm provider

---
+++ ethereum/vmProvider.js
@@ -0,0 +1,50 @@
+import {ethereumRequest} from '../vm-raw'
+
+const JsonRpcProvider = require('./ethers').provider.JsonRpcProvider;
+const defineProperty = require('./ethers').utils.defineProperty;
+
+/**
+ * @desc The VM provider provides an compatible ether.js provider. Calls are passed to the VM and processed there.
+ * @constructor
+ */
+function VMProvider() {
+    if (!(this instanceof VMProvider)){
+        throw new Error(`missing new`);
+    }
+}
+JsonRpcProvider.inherits(VMProvider);
+
+defineProperty(VMProvider.prototype, 'send', function(method, params) {
+
+    const payload = JSON.stringify({
+        method: method,
+        params: params,
+        jsonrpc: "2.0"
+    });
+
+    return new Promise((resolve, reject) => {
+
+        // send request to ethereum
+        ethereumRequest(payload, (response, error) => {
+
+            // reject on error
+            if (!error){
+                return reject(error)
+            }
+
+            // parse response and reject if fails
+            let parsedResponse = {};
+            try {
+                parsedResponse = JSON.parse(response)
+            } catch (err){
+                return reject(error)
+            }
+
+            return resolve(parsedResponse)
+
+        })
+
+    });
+});
+
+module.exports = VMProvider;
--- vm-raw/index.js
+++ vm-raw/index.js
@@ -1,8 +1,12 @@
-const VMRaw = {
+export default {
     /**
      * @desc add a function to the VM a unique reference will be returned
      */
-    registerFunction: global.vmRegisterFunction
-};
-
-export default VMRaw
+    registerFunction: global.vmRegisterFunction,
+    /**
+     * @desc sends an json rpc request to the ethereum network
+     * @param requestObject
+     * @param callback
+     */
+    ethereumRequest: global.ethereumRequest
+}
