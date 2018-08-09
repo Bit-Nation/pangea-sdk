e48a1f8ab0538e92273593498629576ec10957de
Florian Lenz <flenz81@googlemail.com>
2018-08-10 01:38:53 +0300

    [ethereum] nuked ethereum since we shift it to the native side

--- src/ethereum/index.js
+++
@@ -1,9 +0,0 @@
-const Uint8Array = require('typedarray').Uint8Array;
-import {ethereumAddress} from '../vm'
-
-const VMProvider = require('./vmProvider');
-const VMSigner = require('./VMSigner');
-
-const provider = new VMProvider();
-
-export default new VMSigner(provider, ethereumAddress);
